4b0f24c302ddf922cc3998d5d63e747a74e5af54
4Karolis <4karolis.t@gmail.com>
2022-03-17 19:57:32 +0200

    Update Program.cs

--- uzduotys/Program.cs
+++ uzduotys/Program.cs
@@ -4,8 +4,12 @@
     {
         static void Main(string[] args)
         {
-
-
+            double a = 10;
+            double b = 15;
+            
+            Console.Writeline($"Pirma staciojo trikampio krastine: {a}");
+            Console.WriteLine($"Anras staciojo trikampio krastine: {b}");
+            double c = Console.WriteLine($"Staciojo trikampio zambine: {a * a + b * b}");
 
         }
     }
