bcb823f32f2ec14d8a2e1a5be7d2377d342eca9a
4Karolis <4karolis.t@gmail.com>
2022-03-17 20:04:45 +0200

    Update Program.cs

--- uzduotys/Program.cs
+++ uzduotys/Program.cs
@@ -3,7 +3,7 @@
     class Program
     {
         static void Main(string[] args)
-        {
+        {//-------------------------------------------- 3 UZDUOTIS ------------------------------------------
             double a = 10;
             double b = 15;
             
@@ -11,6 +11,16 @@
             Console.WriteLine($"Anras staciojo trikampio krastine: {b}");
             double c = Console.WriteLine($"Staciojo trikampio zambine: {a * a + b * b}");
 
+            //--------------------------------------- 4 UZDUOTIS ---------------------------------------------
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("Viso kintamuju: 3");
+            Console.WriteLine("1 kintamasis: ");
+            Console.WriteLine("2 kintamasis: ");
+            Console.WriteLine("3 kintamasis: ");
+            Console.WriteLine($"Kintamuju suma: {kint1 + kint2 + kint3}");
+            Console.WriteLine($"Kintamuju vidurkis: {(kint1 + kint2 + kint3) / kintSkaicius}" );
+            Console.WriteLine("*************************************************************************");
         }
     }
 }
