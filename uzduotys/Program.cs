3935e3a3dd7a88d04588d127093124151ea07e6e
Arunas <66676626+Fu-Kuro@users.noreply.github.com>
2022-03-17 20:54:54 +0200

    Update Program.cs

--- uzduotys/Program.cs
+++ uzduotys/Program.cs
@@ -5,37 +5,26 @@
         static void Main(string[] args)
         {
             // 1 uzduotsis suskaiciuoti rutulio plota, rezultatas i kintamaji
-            {
+            
                 int spindulys = 6371;
                 double plotas = 4 * 3.1415926535898 * Math.Pow(spindulys, 2);
                 Console.WriteLine($"rutulio plotas = {plotas}");
 
-
-
-
-
-
-
-
-
-
-
-
-
-            }
+            
             // 2 uzduotis
-            {
-                int spindulys = 6371;
+            
+                spindulys = 6371;
                 double turis = (4/3) * 3.1415926535898 * Math.Pow(spindulys, 3);
                 Console.WriteLine($"rutulio plotas = {turis}");
-            }
+            
             //-------------------------------------------- 3 UZDUOTIS ------------------------------------------
             double a = 10;
             double b = 15;
 
-            Console.Writeline($"Pirma staciojo trikampio krastine: {a}");
+            Console.WriteLine($"Pirma staciojo trikampio krastine: {a}");
             Console.WriteLine($"Anras staciojo trikampio krastine: {b}");
-            double c = Console.WriteLine($"Staciojo trikampio zambine: {a * a + b * b}");
+            double c = a * a + b * b;
+            Console.WriteLine($"Staciojo trikampio zambine: {c}");
 
             //--------------------------------------- 4 UZDUOTIS ---------------------------------------------
 
