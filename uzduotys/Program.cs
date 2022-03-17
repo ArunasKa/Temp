ac2037adf56ca99d2017b4c7c656b20a6df52be5
Arunas <66676626+Fu-Kuro@users.noreply.github.com>
2022-03-17 20:04:24 +0200

    Update Program.cs

--- uzduotys/Program.cs
+++ uzduotys/Program.cs
@@ -4,9 +4,22 @@
     {
         static void Main(string[] args)
         {
+            // 1 uzduotsis suskaiciuoti rutulio plota, rezultatas i kintamaji
+            {
+                double plotas;
+                int spindulys = 6371;
+                plotas = 4 * 3.1415926535898 * Math.Pow(spindulys, 2);
+                Console.WriteLine($"rutulio plotas = {plotas}");
 
 
-
+            }
+            // 2 uzduotis
+            {
+                double turis;
+                int spindulys = 6371;
+                turis = (4/3) * 3.1415926535898 * Math.Pow(spindulys, 3);
+                Console.WriteLine($"rutulio plotas = {turis}");
+            }
         }
     }
 }
